e3439dfcf5a1a9835b67b935d3833e6eef3565cf
Ed Orsini <edorsini@Eds-MacBook-Pro.local>
2019-04-16 12:40:28 -0400

    first commit

---
+++ src/duplicateEncode.js
@@ -0,0 +1,29 @@
+function duplicateEncode(word){
+    let result = '';
+
+    for(let i=0; i < word.length; i++) {
+        let current_char = word[i];
+
+        if (['(', ')'].includes(current_char)) {
+            current_char = '\\' + current_char ;
+        }
+
+        let re = new RegExp(current_char,"gi");
+        let char_count =  word.match(re).length;
+
+        if(char_count === 1) {
+            result = `${result}(`;
+        } else if (char_count > 1) {
+            result = `${result})`;
+        }
+    }
+
+    return result;
+}
+
+module.exports = {
+  duplicateEncode: duplicateEncode
+}
+
+
+console.log(duplicateEncode("hello"));
