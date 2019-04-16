85a493339ddafde04c6caae95757592247792ec5
Ed Orsini <edorsini@Eds-MacBook-Pro.local>
2019-04-16 13:37:33 -0400

    cleaning up code

--- test/duplicateEncode.js
+++ test/duplicateEncode.js
@@ -2,29 +2,29 @@ let expect = require('chai').expect;
 const file = require("../src/duplicateEncode.js");
 
 describe('basicTests()', function () {
-  it('should pass the "hello" test', function () {
-    let input = 'hello';
-    let result = file.duplicateEncode(input);
-    expect('(())(').to.be.equal(result);
-  });
+    it('should pass the "hello" test', function () {
+        let input = 'hello';
+        let result = file.duplicateEncode(input);
+        expect('(())(').to.be.equal(result);
+    });
 
-  it('should pass the "din" test', function () {
-    let input = 'din';
-    let result = file.duplicateEncode(input);
-    expect('(((').to.be.equal(result);
-  });
+    it('should pass the "din" test', function () {
+        let input = 'din';
+        let result = file.duplicateEncode(input);
+        expect('(((').to.be.equal(result);
+    });
 
-  it('should pass the "recede" test', function () {
-    let input = 'recede';
-    let result = file.duplicateEncode(input);
-    expect('()()()').to.be.equal(result);
-  });
+    it('should pass the "recede" test', function () {
+        let input = 'recede';
+        let result = file.duplicateEncode(input);
+        expect('()()()').to.be.equal(result);
+    });
 
-  it('should pass the "Success" test ignorning the case', function () {
-    let input = 'Success';
-    let result = file.duplicateEncode(input);
-    expect(')())())').to.be.equal(result);
-  });
+    it('should pass the "Success" test ignorning the case', function () {
+        let input = 'Success';
+        let result = file.duplicateEncode(input);
+        expect(')())())').to.be.equal(result);
+    });
 });
 
 
